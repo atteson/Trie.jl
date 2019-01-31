a4762a7cc00aa0e67413b44e6f2aa942bed6a2fd
Kevin Atteson <kevin@atteson.com>
2019-01-30 20:04:20 -0500

    misc

--- src/Tries.jl
+++ src/Tries.jl
@@ -4,60 +4,64 @@ export Trie
 
 # The Trie's in DataStructures.jl only seem to allow strings for keys
 # We're going to assume Vector{Int} for keys and fixed maximum in each dimension
-mutable struct Trie{V,N <: Integer, T} <: AbstractArray{V,N}
+mutable struct Trie{V,T,N} <: AbstractArray{V,N}
     size::NTuple{N,Int}
     default::V
     data::Vector{Union{Missing,T}}
 end
 
-Base.size( t::Trie{V,N} ) where {V,T,N} = t.size
+Base.size( t::Trie{V,T,N} ) where {V,T,N} = t.size
 
-struct TrieNode{V,N,T}
+struct TrieNode{T}
     data::Vector{Union{Missing,T}}
 end
 
-# the code below assumes there are at least 2 levels
+TrieType( V::DataType, N::Int ) =
+    N == 0 ? V : TrieType( TrieNode{V}, N-1 )
+
 Trie( default::V, size::Int... ) where {V} =
-    Trie( size, default, Vector{Union{Missing,TrieNode{V,length(size)-1}}}(missing, size[1]) )
+    Trie( size, default, Vector{Union{TrieType( V, length(size)-1 ),Missing}}(missing, size[1]) )
 
-@inline function Base.setindex!( head::Trie{V,N}, v::V, indices::Int... ) where {V,N}
+Base.copy( head::Trie{V,TrieNode{T},N} ) where {V,T,N} =
+    Trie( head.size, head.default, copy( head.data ) )
+
+Base.copy( node::TrieNode{T} ) where {T} =
+    TrieNode( copy( node.data ) )
+
+@inline function Base.setindex!( head::Trie{V,TrieNode{T},N}, v::V, indices::Int... ) where {V,T,N}
     if ismissing(head.data[indices[1]])
-        head.data[indices[1]] = TrieNode( Vector{Union{Missing,TrieNode{V,N-1}}}( missing, head.size[1] ) )
+        head.data[indices[1]] = TrieNode( Vector{Union{Missing,T}}( missing, head.size[1] ) )
     end
-    set!( head.data[indices[1]], head.size, v, indices... )
+    set!( head.data[indices[1]], head.size, v, 2, indices... )
 end
 
-@inline function set!( node::TrieNode{V,M}, size::NTuple{N,Int}, v::V, indices::Int... ) where {V,M,N}
-    if ismissing( node.data[indices[N-M]] )
-        node.data[indices[n]] = TrieNode( Vector{Union{Missing,TrieNode{V,M-1}}}( missing, size[N-M] ) )
+@inline function set!( node::TrieNode{TrieNode{T}}, size::NTuple{N,Int}, v::V, n::Int, indices::Int... ) where {V,T,N}
+    if ismissing( node.data[indices[n]] )
+        node.data[indices[n]] = TrieNode( Vector{Union{Missing,T}}( missing, size[n] ) )
     end
-    set!( node.data[indices[N-M]], size, v, indices... )
+    set!( node.data[indices[n]], size, v, n+1, indices... )
 end
 
-@inline function set!( node::TrieNode{V,1}, size::NTuple{N,Int}, v::V, indices::Int... ) where {V,N}
-    if ismissing( node.data[indices[N-1]] )
-        node.data[indices[N-1]] = TrieNode( Vector{Union{Missing,V}}( missing, size[N] ) )
-    end
-    node.data[indices[N-1]][indices[N]] = v
-end
+@inline set!( node::TrieNode{V}, size::NTuple{N,Int}, v::V, n::Int, indices::Int... ) where {V,N} =
+    node.data[indices[n]] = v
 
-@inline function Base.getindex( head::Trie{V,N}, indices::Int... ) where {V,N}
+@inline function Base.getindex( head::Trie{V,T,N}, indices::Int... ) where {V,T,N}
     if ismissing( head.data[indices[1]] )
         return head.default
     else
-        return get( head.data[indices[1]], head.default, indices... )
+        return get( head.data[indices[1]], head.default, 2, indices... )
     end
 end
 
-@inline function get( node::TrieNode{TrieNode{V,M}}, v::V, indices::Int... ) where {V,M}
-    if ismissing( node.data[indices[length(indices)-M]] )
+@inline function get( node::TrieNode{TrieNode{T}}, v::V, n::Int, indices::Int... ) where {T,V}
+    if ismissing( node.data[indices[n]] )
         return v
     else
-        return get( node.data[indices[length(indices)-M]], v, indices... )
+        return get( node.data[indices[n]], v, n+1, indices... )
     end
 end
 
-@inline get( node::TrieNode{V,1}, v::V, indices::Int... ) where {V} =
-    ismissing( node.data[indices[end]] ) ? v : node.data[indices[end]]
+@inline get( node::TrieNode{T}, v::V, n::Int, indices::Int... ) where {T,V} =
+    ismissing( node.data[indices[n]] ) ? v : node.data[indices[n]]
 
 end # module
